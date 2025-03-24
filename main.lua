3279db84f5f453b242a1db5c6e19b81ea9ab74b9
Babouye <esteban.bouyault-yvanez@epitech.eu>
2025-03-24 20:10:26 +0100

    refactor: upgrade visibility of implementation to main.lua file

--- main.lua
+++ main.lua
@@ -5,19 +5,28 @@ function love.load()
     love.window.setFullscreen(true)
     love.window.setTitle("on choisira plus tard.")
     love.graphics.setBackgroundColor(0.2, 0.2, 0.2)
+
+    -- Player --
     player.load()
+    ------------
 end
 
 function love.update(dt)
     if love.keyboard.isDown("escape") then
         love.event.quit()
     end
+
+    -- Player --
     player.update(dt)
     attack.update(dt)
+    ------------
 end
 
 function love.draw()
     love.graphics.setColor(1, 1, 1)
+
+    -- Player --
     player.draw()
     attack.draw()
+    ------------
 end
