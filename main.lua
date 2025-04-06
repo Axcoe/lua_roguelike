3c6ac0a5720263bf2ad7009fe9b1f1f48ff84daf
Babouye <esteban.bouyault-yvanez@epitech.eu>
2025-04-06 23:28:05 +0200

    refactor: store main overlines in subfunctions to clean it

--- main.lua
+++ main.lua
@@ -3,13 +3,16 @@ local attack = require("player/attack")
 local target = require("target/target")
 
 function love.load()
-    love.window.setMode(love.graphics.getWidth(),
-                        love.graphics.getHeight(), 
-                        {
-                            fullscreen = true,
-                            display = 2,
-                            resizable = false
-                        })
+    love.window.setMode
+    (
+        love.graphics.getWidth(),
+        love.graphics.getHeight(),
+        {
+            fullscreen = true,
+            display = 2,
+            resizable = false
+        }
+    )
 
     love.window.setTitle("on choisira plus tard.")
     love.graphics.setBackgroundColor(0.2, 0.2, 0.2)
@@ -19,39 +22,35 @@ function love.load()
     ------------
 end
 
-function love.update(dt)
-    if love.keyboard.isDown("escape") then
+function love.keypressed(key)
+    if key == "escape" then
         love.event.quit()
     end
+    target.keypressed(key)
+end
 
+function love.update(dt)
     -- Player --
     player.update(dt)
     attack.update(dt)
     ------------
 
-    -- Is hit target ? --
-    for i = #attack.bullets, 1, -1 do
-        local bullet = attack.bullets[i]
-        if target.isAlive() and target.isHit(bullet) then
-            target.hit()
-            table.remove(attack.bullets, i)
-        end
-    end
-
+    target.update(dt)
 end
 
 function love.draw()
     love.graphics.setColor(1, 1, 1)
 
+    --------- Player ----------
     if player.hp <= 0 then
-        love.graphics.setColor(1, 0, 0)
-        love.graphics.setFont(love.graphics.newFont(64))
-        love.graphics.printf("Game Over", 0, love.graphics.getHeight() / 2 - 64, love.graphics.getWidth(), "center")
-    else        
-        -- Player --
+        player.gameOver()
+    else
         player.draw()
         attack.draw()
+    ----------------------------
+
+    ---------- Target ----------
         target.draw()
-        ------------
+    ----------------------------
     end
 end
