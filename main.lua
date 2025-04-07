9ff8d3fe7d8365b32eccec53c740d98a9e1354b8
Alice Duvillier <alice.duvillier@epitech.eu>
2025-04-07 18:36:34 +0200

    up: button main good, they can do functions when i click on

--- main.lua
+++ main.lua
@@ -1,7 +1,11 @@
+local start_menu = require("menus/start_menu")
+
 function love.load()
     love.window.setFullscreen(true)
     love.window.setTitle("on choisira plus tard.")
     love.graphics.setBackgroundColor(0.2, 0.2, 0.2)
+
+    start_menu.loadStartMenu()
 end
 
 function love.update(dt)
@@ -13,4 +17,6 @@ end
 function love.draw()
     love.graphics.setColor(1, 1, 1)
     love.graphics.print("Coucou :D", 300, 250)
+
+    start_menu.drawStartMenu()
 end
